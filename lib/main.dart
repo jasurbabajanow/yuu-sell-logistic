ab6d42716c1df3f89fb46944a3c3824b952a9947
Jasur Babajanow <115019821+jasurbabajanow@users.noreply.github.com>
2025-12-04 16:01:01 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: AppColors.main),
       ),
-      home: SignUpPage(),
+      home: MainPage(),
     );
   }
 }
