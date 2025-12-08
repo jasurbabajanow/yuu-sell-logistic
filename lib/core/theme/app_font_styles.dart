135ded5f3f0aaa502a2e58c508f593c4145dd0cf
mahmutiLaman <mmilamanov@gmail.com>
2025-12-09 02:15:12 +0500

    poppins font style added via google_fonts

--- lib/core/theme/app_font_styles.dart
+++ lib/core/theme/app_font_styles.dart
@@ -1,30 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppFontStyles {
-  static TextStyle s8w500(double ratio) =>
-      TextStyle(fontWeight: FontWeight.w500, fontSize: 8 * ratio);
+  static TextStyle s8w500(double ratio) => GoogleFonts.poppins(
+        fontWeight: FontWeight.w500,
+        fontSize: 8 * ratio,
+      );
 
-  static TextStyle s12w600(double ratio) =>
-      TextStyle(fontWeight: FontWeight.w600, fontSize: 12 * ratio);
+  static TextStyle s12w600(double ratio) => GoogleFonts.poppins(
+        fontWeight: FontWeight.w600,
+        fontSize: 12 * ratio,
+      );
 
-  static TextStyle s12w400(double ratio) =>
-      TextStyle(fontWeight: FontWeight.w400, fontSize: 12 * ratio);
+  static TextStyle s12w400(double ratio) => GoogleFonts.poppins(
+        fontWeight: FontWeight.w400,
+        fontSize: 12 * ratio,
+      );
 
-  static TextStyle s14w700(double ratio) =>
-      TextStyle(fontWeight: FontWeight.w700, fontSize: 14 * ratio);
+  static TextStyle s14w700(double ratio) => GoogleFonts.poppins(
+        fontWeight: FontWeight.w700,
+        fontSize: 14 * ratio,
+      );
 
-  static TextStyle s16w500(double ratio) =>
-      TextStyle(fontWeight: FontWeight.w500, fontSize: 16 * ratio);
+  static TextStyle s16w500(double ratio) => GoogleFonts.poppins(
+        fontWeight: FontWeight.w500,
+        fontSize: 16 * ratio,
+      );
 
-  static TextStyle s16w400(double ratio) =>
-      TextStyle(fontWeight: FontWeight.w400, fontSize: 16 * ratio);
+  static TextStyle s16w400(double ratio) => GoogleFonts.poppins(
+        fontWeight: FontWeight.w400,
+        fontSize: 16 * ratio,
+      );
 
-  static TextStyle s16w600(double ratio) =>
-      TextStyle(fontWeight: FontWeight.w600, fontSize: 16 * ratio);
+  static TextStyle s16w600(double ratio) => GoogleFonts.poppins(
+        fontWeight: FontWeight.w600,
+        fontSize: 16 * ratio,
+      );
 
-  static TextStyle s24w600(double ratio) =>
-      TextStyle(fontWeight: FontWeight.w600, fontSize: 24 * ratio);
+  static TextStyle s24w600(double ratio) => GoogleFonts.poppins(
+        fontWeight: FontWeight.w600,
+        fontSize: 24 * ratio,
+      );
 
-  static TextStyle s26w600(double ratio) =>
-      TextStyle(fontWeight: FontWeight.w600, fontSize: 26 * ratio);
+  static TextStyle s26w600(double ratio) => GoogleFonts.poppins(
+        fontWeight: FontWeight.w600,
+        fontSize: 26 * ratio,
+      );
 }
